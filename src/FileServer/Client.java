b6dd557e5af4734e5d8942e0424335684f44482e
Denis <deniskkaa9@gmail.com>
2023-03-14 18:36:24 +0300

    First attempt, working ok but 1 thread(

---
+++ src/FileServer/Client.java
@@ -0,0 +1,161 @@
+package FileServer;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.SocketException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Client {
+
+    private static boolean isRunning = true;
+    public static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) throws IOException, SocketException {
+        String address = "127.0.0.1";
+        int port = 34522;
+        Socket socket = new Socket(address,port);
+        DataInputStream input = new DataInputStream(socket.getInputStream());
+        DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+        String req = "";
+        String response = "";
+        byte[] data;
+        String filename = "";
+
+        while (isRunning) {
+            System.out.println("Enter action (1 - get a file, 2 - save a file, 3 - delete a file): ");
+            String action = scan.nextLine();
+            switch (action) {
+                case "1":
+                    req = getRequest();
+                    output.writeUTF(req);
+                    System.out.println("The request was sent.");
+                    response = input.readUTF();
+                    if (processResponse(response, action)) {
+                        int length = input.readInt();
+                        data = new byte[length];
+                        input.readFully(data, 0, data.length);
+                        filename = "D:\\JavaFiles\\data\\" + scan.nextLine();
+                        try (FileOutputStream fos = new FileOutputStream(filename)) {
+                            fos.write(data);
+                            System.out.println(filename + "****");
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.print("File saved on the hard drive!");
+                    }
+                    break;
+
+                case "2":
+                    System.out.print("Enter filename: ");
+                    String name = scan.nextLine();
+                    Path path = Paths.get("D:\\JavaFiles\\data\\" + name);
+                    data = Files.readAllBytes(path);
+                    System.out.print("Enter name of the file to be saved on server: ");
+                    String outName = scan.nextLine();
+                    output.writeUTF("PUT" + ";" + outName);
+                    output.writeInt(data.length); // write length of the message
+                    output.write(data);
+                    response = input.readUTF();
+                    processResponse(response, action);
+                    break;
+
+                case "3":
+                    req = deleteRequest();
+                    output.writeUTF(req);
+                    System.out.println("The request was sent.");
+                    response = input.readUTF();
+                    processResponse(response, action);
+                    break;
+                case "exit":
+                    output.writeUTF("exit");
+                    output.flush();
+                    output.close();
+                    input.close();
+                    socket.close();
+                    System.exit(0);
+                }
+            }
+        }
+        
+
+
+
+
+    public static String getRequest() {
+        String name = "";
+        String mode = "";
+        System.out.println("Do you want to get the file by/id (1 - name, 2 - id): ");
+        String choice = scan.nextLine();
+        switch (choice) {
+            case "1":
+                System.out.print("Enter filename: ");
+                name = scan.nextLine();
+                mode = "n";
+                break;
+            case "2":
+                System.out.print("Enter id: ");
+                name = scan.nextLine();
+                mode = "i";
+                break;
+        }
+        return "GET" + ";" + mode + ";" + name;
+    }
+
+    public static String deleteRequest() {
+        String name = "";
+        String mode = "";
+        System.out.println("Do you want to delete the file by name/id (1 - name, 2 - id): ");
+        String choice = scan.nextLine();
+        switch (choice) {
+            case "1":
+                System.out.print("Enter filename: ");
+                name = scan.nextLine();
+                mode = "n";
+                break;
+            case "2":
+                System.out.print("Enter id: ");
+                name = scan.nextLine();
+                mode = "i";
+                break;
+        }
+        return "DELETE" + ";" + mode + ";" + name;
+    }
+
+    public static Boolean processResponse(String response, String operation) {
+        switch (operation) {
+            case "2":
+                if (response.startsWith("200")) {
+                    System.out.println("Response says that file is saved! ID = " + response.substring(4));
+                    return true;
+                }
+                else {
+                    System.out.println("The response says that creating the file was forbidden!");
+                }
+                break;
+            case "1":
+                if (response.equals("200"))  {
+                    System.out.print("The file was downloaded! Specify a name for it: ");
+                    return true;
+                }
+                else {
+                    System.out.println("The response says that this file is not found!");
+                }
+                break;
+            case "3":
+                if (response.equals("200")) {
+                    System.out.println("The response says that the file was successfully deleted!");
+                    return true;
+                }
+                else {
+                    System.out.println("The response says that this file is not found!");
+                }
+                break;
+        }
+        return false;
+    }
+
+}
---
+++ src/FileServer/Server.java
@@ -0,0 +1,175 @@
+package FileServer;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+
+public class Server {
+
+    static String path = "D:\\JavaFiles\\data\\";
+    private static boolean isRunning = true;
+    private static final int PORT = 34522;
+    public static HashMap<String, String> filenames = new HashMap<String, String>();
+
+
+
+    public static void main(String[] args) throws IOException {
+
+        createDirectory(path);
+
+        try (ServerSocket server = new ServerSocket(PORT)) {
+            System.out.println("Сервер запущен и готов работать.");
+
+            while (isRunning) {
+                String response = "";
+                String id = "";
+                try (
+                        Socket socket = server.accept();
+                        DataInputStream input = new DataInputStream(socket.getInputStream());
+                        DataOutputStream output = new DataOutputStream(socket.getOutputStream())
+                ) {
+                    String message = input.readUTF();
+                    if (message.equals("exit")) {
+                        input.close();
+                        output.close();
+                        socket.close();
+                        isRunning = false;
+                    } else{
+                        switch ((message.split(";")[0])){
+
+                            case "PUT":
+                                System.out.println(message);
+                                if (message.split(";").length > 1) {
+                                    path = path + message.split(";")[1];
+                                }
+                                File f = new File(path);
+                                if(f.exists() && !f.isDirectory()) {
+                                    response = "403";
+                                } else {
+                                    int length = input.readInt();
+                                    byte[] data = new byte[length];
+                                    input.readFully(data, 0, data.length);
+                                    System.out.println(path);
+                                    String hash = generateId(path);
+                                    filenames.put(hash, path);
+                                    try (FileOutputStream fos = new FileOutputStream(path)) {
+                                        fos.write(data);
+                                        System.out.println(path + "****");
+                                    } catch (FileNotFoundException e) {
+                                        e.printStackTrace();
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+                                    id = hash;
+                                    response = "200" + " " + id;
+                                }
+                                output.writeUTF(response);
+                                break;
+
+
+                            case "GET":
+                                if (message.split(";")[1].equals("n")) {
+                                    path = path + message.split(";")[2];
+                                    f = new File(path);
+                                    if (f.exists() && !f.isDirectory()) {
+                                        response = "200";
+                                        Path path2 = Paths.get(path);
+                                        byte[] data = new byte[0];
+                                        if (Files.exists(path2) && !Files.isDirectory(path2)) {
+                                            try {
+                                                data = Files.readAllBytes(path2);
+                                            } catch (IOException e) {
+                                                e.printStackTrace();
+                                            }
+                                        }
+                                        output.writeUTF(response);
+                                        output.writeInt(data.length);
+                                        output.write(data);
+                                    } else {
+                                        response = "403";
+                                        output.writeUTF(response);
+                                    }
+                                } else {
+                                    id = message.split(";")[2];
+                                    if (filenames.containsKey(id)) path = filenames.get(id);
+                                    f = new File(path);
+                                    if (f.exists() && !f.isDirectory()) {
+                                        response = "200";
+                                        Path path2 = Paths.get(path);
+                                        byte[] data = new byte[0];
+                                        if (Files.exists(path2) && !Files.isDirectory(path2)) {
+                                            try {
+                                                data = Files.readAllBytes(path2);
+                                            } catch (IOException e) {
+                                                e.printStackTrace();
+                                            }
+                                        }
+                                        output.writeUTF(response);
+                                        output.writeInt(data.length);
+                                        output.write(data);
+                                    } else {
+                                        response = "403";
+                                        output.writeUTF(response);
+                                    }
+                                }
+                                break;
+
+
+                            case "DELETE":
+                                if (message.split(";")[1].equals("n")){
+                                    path = path + message.split(";")[2];
+                                } else {
+                                    id = message.split(";")[2];
+                                    if (filenames.containsKey(id)) path = filenames.get(id);
+                                }
+                                f = new File(path);
+                                if (f.exists() && !f.isDirectory()) {
+                                    response = "200";
+                                    delete(path);
+                                } else {
+                                    response = "403";
+                                }
+                                output.writeUTF(response);
+                                break;
+                        }
+                        output.flush();
+                    }
+
+
+
+                }
+            }
+        }
+
+    }
+    public static void createDirectory(String path) {
+        File fileDirectory = new File(path);
+        if (!fileDirectory.exists()) {
+            fileDirectory.mkdirs();
+        }
+    }
+
+    public static String generateId(String filename) {
+        return String.valueOf(Math.abs(filename.hashCode()));
+    }
+
+    public static void delete(String filepath) {
+        try {
+            File file = new File(filepath);
+            file.delete();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
+
+
+
+
+
